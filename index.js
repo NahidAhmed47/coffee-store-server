72ff2c962de6b0f4103f0730714133edfc56fa18
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-05-09 10:10:59 +0600

    evvironment variable setup

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
 
-// coffee-master
-// 644CK6393iRKq1o3
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://coffee-master:644CK6393iRKq1o3@cluster0.ze0g6j8.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ze0g6j8.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
