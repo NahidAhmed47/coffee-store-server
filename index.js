6dff4179e133420892e496e5b46d31ce9b09b540
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-05-09 11:48:03 +0600

    delete coffee successfully

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ze0g6j8.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,12 +23,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeCollection = client.db("coffeeDB").collection("coffee");
+    app.post('/coffee',async(req, res)=>{
+      const newCoffee = req.body;
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+    app.get('/coffee',async(req, res)=>{
+      const result = await coffeeCollection.find().toArray();
+      res.send(result);
+    })
+    app.delete('/coffee/:id',async(req, res)=>{
+      const id = req.params.id;
+      const result = await coffeeCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
